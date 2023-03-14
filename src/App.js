697b02c64a301cebb65b3a495a82a69ebdc1ef63
Rustam Guseinov <rg20031991.com>
2023-03-14 11:13:06 +0300

    fixing the location of components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./Home";
+import Home from "./components/Home";
 import "./index.css";
 import UserDetails from "./UserDetails";
 import Users from "./Users";
