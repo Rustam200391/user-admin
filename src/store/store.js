dc2003282198e462e45718c6dbcc0a06fe4d5c94
Rustam Guseinov <rg20031991.com>
2023-03-15 12:13:48 +0300

    placing store components in folder of same name

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./store/slice";
+import userReducer from "./slice";
 const store = configureStore({
   reducer: {
     user: userReducer
